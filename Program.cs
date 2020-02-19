0358ff81af50be0043949e31664ebd300cf7753d
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2020-02-19 16:59:51 -0500

    bot can now search for hastags, and scrape data from other accounts

--- Program.cs
+++ Program.cs
@@ -11,30 +11,52 @@ namespace happyBirthday
             options.AddArguments("--disable-gpu");
 
             var chromeDriver = new ChromeDriver(options);
-            //driver.manage().timeouts().implicitlyWait()
 
-
-
+            //go to site
             chromeDriver.Navigate().GoToUrl("https://www.instagram.com/accounts/login/?source=auth_switcher");
+            //wait for page to load
+            chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 
-            chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-
-            //chromeDriver.FindElementByXPath("//*[@id=\"react - root\"]/section/main/article/div[2]/div[2]/p/a").Click();
-
-            chromeDriver.FindElementByName("username").SendKeys("//username//");
+            //login
+            chromeDriver.FindElementByName("username").SendKeys("charliethegooddoge");
             chromeDriver.FindElementByName("password").SendKeys("//password//");
 
-            //chromeDriver.FindElementByClassName("password").SendKeys(ConsoleKey.Enter);
-
-
-
+            //exit notifications pop up on login
             chromeDriver.FindElementByClassName("DhRcB").Click();
             chromeDriver.FindElementByClassName("HoLwm").Click();
 
-            //search bar
-            chromeDriver.FindElementByClassName("XTCLo").SendKeys("healthy snack");
+            //search for items here
+            chromeDriver.FindElementByClassName("XTCLo").SendKeys("#dogsofinstagram");
+
+            //click on hashtag you searched for
+            chromeDriver.FindElementByClassName("z556c").Click();
+
+            //click first photo
+            chromeDriver.FindElementByClassName("_bz0w").Click();
+            //continue to first photo user's page
+            chromeDriver.FindElementByClassName("e1e1d").Click();
+
+            //wait 5 secs
+            chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            //gather user's information and print to console
+            string name = chromeDriver.FindElementByClassName("_7UhW9").Text;
+            Console.WriteLine("Name: " + name);
+
+            string posts = chromeDriver.FindElementByClassName("g47SY").Text;
+            Console.WriteLine("number of posts: " + posts);
+
+            string bio = chromeDriver.FindElementByClassName("-vDIg").Text;
+            Console.WriteLine("Bio: " + bio);
 
 
+            //int test = Int32.Parse(followers);
+            //Console.WriteLine("test: " + test);
+
+            //click first photo on users page and comment
+            chromeDriver.FindElementByClassName("_9AhH0").Click();
+            chromeDriver.FindElementByClassName("RxpZH").SendKeys(name + "you're so cute!");
+            chromeDriver.FindElementByClassName("X7cDz").Click();
 
 
 
